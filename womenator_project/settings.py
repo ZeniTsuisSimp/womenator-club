02d49d9b5467e5bf546c911a50200469f5bafb43
Aman <138551220+ZeniTsuisSimp@users.noreply.github.com>
2026-03-10 20:09:43 +0530

    Update ALLOWED_HOSTS for production environment

--- womenator_project/settings.py
+++ womenator_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-nk)(q4pmve*&pb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'zenitsuissimp-womenovators-club.hf.space').split(',')
 
 CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')]
 
